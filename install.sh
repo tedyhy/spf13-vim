79341d551b54cf50b97c3feb9b9d5ed4e048571f
tedyhy <916610351@qq.com>
2017-11-30 17:59:01 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -15,10 +15,10 @@
 #   limitations under the License.
 
 ############################  SETUP PARAMETERS
-app_name='spf13-vim'
-[ -z "$APP_PATH" ] && APP_PATH="$HOME/.spf13-vim-3"
-[ -z "$REPO_URI" ] && REPO_URI='https://github.com/spf13/spf13-vim.git'
-[ -z "$REPO_BRANCH" ] && REPO_BRANCH='3.0'
+app_name='tedyhy-vim'
+[ -z "$APP_PATH" ] && APP_PATH="$HOME/.tedyhy-vim-6"
+[ -z "$REPO_URI" ] && REPO_URI='https://github.com/tedyhy/tedyhy-vim.git'
+[ -z "$REPO_BRANCH" ] && REPO_BRANCH='6.0'
 debug_mode='0'
 fork_maintainer='0'
 [ -z "$VUNDLE_URI" ] && VUNDLE_URI="https://github.com/gmarik/vundle.git"
